e17732ac758400cd76a2efda56615b356eded89b
JoshuaManto <joshuamanto@yahoo.com>
2019-11-23 13:43:35 -0800

    Refactored schema and resolvers to individual files

--- src/index.js
+++ src/index.js
@@ -2,6 +2,9 @@ const express = require("express");
 const graphqlHTTP = require("express-graphql");
 const { graphql, buildSchema } = require("graphql");
 
+const schema = require("./schema.js");
+const resolvers = require("./resolvers.js");
+
 const PORT = 3000;
 
 const app = express();
@@ -24,50 +27,103 @@ const app = express();
 //   }
 // `);
 
-const schema = buildSchema(`
-  type Query {
-    info: String!
-    feed: [Link]!
-  }
+// const schema = buildSchema(`
+//   type Query {
+//     info: String!
+//     feed: [Link]!
+//   }
 
-  type Link {
-    id: ID!
-    description: String!
-    url: String!
-  }
-`);
+//   type Mutation {
+//     post(url: String!, descripton: String!): Link!
+//   }
 
-let links = [
-  {
-    id: "link-0",
-    url: "www.howtographql.com",
-    description: "Fullstack tutorial for GraphQL"
-  }
-];
+//   type Link {
+//     id: ID!
+//     description: String!
+//     url: String!
+//   }
+// `);
+
+// let links = [
+//   {
+//     id: "link-0",
+//     url: "www.howtographql.com",
+//     description: "Fullstack tutorial for GraphQL"
+//   }
+// ];
+// let linksCount = links.length;
 
 // Resolver
-const resolver = {
-  info: () => {
-    // return null;
-    return "This is an API of a Hackernews Clone!";
-  },
+// const resolvers = {
+//   info: () => {
+//     // return null;
+//     return "This is an API of a Hackernews Clone!";
+//   },
 
-  feed: () => {
-    return links;
-  },
+//   feed: () => {
+//     return links;
+//   },
 
-  Link: {
-    id: parent => parent.id,
-    description: parent => parent.description,
-    url: parent => parent.url
-  }
-};
+//   link: args => {
+//     console.log(args);
+
+//     for (let link of links) {
+//       console.log(link);
+//       if (link.id === args.id) {
+//         return link;
+//       }
+//     }
+//     throw new Error("no link exists with id ", args.id);
+//   },
+
+//   post: args => {
+//     const link = {
+//       id: `link-${linksCount++}`,
+//       description: args.description,
+//       url: args.url
+//     };
+//     links.push(link);
+//     return link;
+//   },
+
+//   update: args => {
+//     console.log(args);
+//     for (let i = 0; i < links.length; i++) {
+//       if (args.id === links[i].id) {
+//         links[i].url = args.url;
+//         links[i].description = args.description;
+
+//         return links[i];
+//       }
+//     }
+//     throw new Error("no link exists with id ", args.id);
+//   },
+
+//   delete: args => {
+//     for (let i = 0; i < links.length; i++) {
+//       if (args.id === links[i].id) {
+//         const copy = links[i];
+
+//         links.splice(i, 1);
+//         console.log(links);
+//         return copy;
+//       }
+//     }
+//     throw new Error("no link exists with id ", args.id);
+//   },
+
+//   Link: {
+//     id: parent => parent.id,
+//     description: parent => parent.description,
+//     url: parent => parent.url
+//   }
+// };
 
 app.use(
   "/graphql",
   graphqlHTTP({
     schema: schema,
-    rootValue: resolver,
+    rootValue: resolvers,
     graphiql: true
   })
 );
